f15cb7647917bd93a53612992df84e15b9166a06
GuanXin Wang <Alex0972112871@gmail.com>
2021-09-02 13:24:02 +0800

    heroku doesn't recognize the windows path

--- vaccination.py
+++ vaccination.py
@@ -23,10 +23,10 @@ def vaccination_lastest_date_by_country(dataframe):
   '''group the data by location and get last date data'''
   return dataframe.groupby('location').last().reset_index()
 
-vaccination_data = pd.read_csv(current_directory +'\\vaccinations.csv')
+vaccination_data = pd.read_csv('vaccinations.csv')
 df = pd.DataFrame(vaccination_data)
 
-imf_data = pd.read_csv(current_directory + '\\imf-who-covid-19-vaccine-supply-tracker.csv',skiprows=2, engine='python')
+imf_data = pd.read_csv('imf-who-covid-19-vaccine-supply-tracker.csv',skiprows=2, engine='python')
 df_imf = pd.DataFrame(imf_data)
 
 country_lastest_vaccination = vaccination_lastest_date_by_country(df)
